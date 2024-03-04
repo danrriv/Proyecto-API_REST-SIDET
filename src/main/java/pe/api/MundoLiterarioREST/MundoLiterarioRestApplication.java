c0ad526090a1e9a4ea0165dcd0b5d4cfcea07135
Daniel Rodriguez <dan74109@gmail.com>
2024-03-03 21:10:11 -0500

    Iniciando Proyecto

---
+++ src/main/java/pe/api/MundoLiterarioREST/MundoLiterarioRestApplication.java
@@ -0,0 +1,13 @@
+package pe.api.MundoLiterarioREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundoLiterarioRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundoLiterarioRestApplication.class, args);
+	}
+
+}
